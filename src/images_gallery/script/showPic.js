f8d015e5f4a057244d14a1661100a0e9de5f8f8e
ryp <yinrunping@appvantage.com.cn>
2018-06-27 13:32:44 +0800

    image gallery

---
+++ src/images_gallery/script/showPic.js
@@ -0,0 +1,19 @@
+function showPic(whichpic){
+    const source = whichpic.getAttribute('href');
+    let placeholder = document.getElementById('placeholder');
+    // placeholder.src = source;// 老办法，特定属性才可以使用这种方法直接赋值, 只适应于web文档
+    placeholder.setAttribute('src',source);// 通用方法。适用于任务属性，多种环境，多种程序设计语言
+
+    const text = whichpic.getAttribute('title');
+    let description = document.getElementById('description');
+    description.firstChild.nodeValue = text;
+}
+function countBodyChildren(){
+    const body_element = document.getElementsByTagName('body')[0];
+    console.log(body_element.nodeType);
+    // nodeType value: 
+    // 1 : element node
+    // 2 : attribute node
+    // 3 : text node
+}
+window.onload = countBodyChildren;
